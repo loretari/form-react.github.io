5d9f5e5d84dabfb70f0bbf2ded81fc535782a7e6
loretari <loretari@gmail.com>
2023-07-21 14:14:00 +0300

    init: initial first commit

---
+++ form/src/Form.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default function Form() {
+
+    const [formData, setFormData] = React.useState(
+        {
+            firstName: "",
+            lastName: "",
+            email: "",
+            comments: "",
+            isFriendly: true,
+            employment: ""
+        }
+    )
+
+    function handlerChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.value]: event.target.name
+            }
+        })
+    }
+
+    return (
+        <form>
+            <input
+                type= "text"
+                placeholder= "First Name"
+                onChange= {handlerChange}
+                name= "firstName"
+                value={formData.firstName}
+                />
+            <input
+                type= "text"
+                placeholder= "Last Name"
+                onChange= {handlerChange}
+                name= "lastName"
+                value={formData.lastName}
+            />
+            <input
+                type= "email"
+                placeholder= "Email"
+                onChange= {handlerChange}
+                name= "email"
+                value={formData.email}
+            />
+
+        </form>
+    )
+}
