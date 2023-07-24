464aef053714451ac4d78d7f9383066a5724b77b
loretari <loretari@gmail.com>
2023-07-24 09:34:25 +0300

    init: add Sign Form

--- form/src/index.js
+++ form/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 
 import Form from './Form';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <Form />
+//     </React.StrictMode>
+// );
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
     <React.StrictMode>
-        <Form />
+        <App />
     </React.StrictMode>
 );
